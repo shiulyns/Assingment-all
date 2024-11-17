e69855efbc076a4edc14022fa6a37350afac1ef9
shiulyns <shiulybegumnila@gmail.com>
2024-11-17 06:44:04 -0800

    nav bar

--- p1/javvaScript.js
+++
@@ -1,6 +0,0 @@
-const menuIcon = document.querySelector(".menu-icon");
-const navbar = document.getElementById("navbar");
-
-menuIcon.addEventListener("click", () => {
-  navbar.classList.toggle("active");
-});
